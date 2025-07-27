a9af8df9126fde1f9a0cab072a04c0fe93b34190
willian-mayer <willian.mayer.br@gmail.com>
2025-07-27 18:15:38 -0300

    resizing entrance

--- src/components/Entrance.tsx
+++ src/components/Entrance.tsx
@@ -12,10 +12,10 @@ export default function Entrance() {
   return (
     <div className="h-screen bg-black flex items-center justify-center relative w-screen">
       {/* Logo en el centro */}
-      <img src="/logo-entrance.png" alt="logo" className="h-128 w-auto" />
+      <img src="/logo-entrance.png" alt="logo" className="h-72 lg:h-96 w-auto" />
 
       {/* Idiomas en la esquina superior derecha, en vertical */}
-      <div className="absolute top-4 right-4 flex flex-col items-enter space-y-2 text-white">
+      <div className="absolute top-10 right-10 flex flex-col items-enter space-y-2 text-white">
         {languages.map((lang) => (
           <div key={lang.short} className="flex items-center space-x-2 hover:underline cursor-pointer">
             <img src="/item.png" alt="icon" className="h-4 w-auto" />
