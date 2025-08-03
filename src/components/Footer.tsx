d3de090f62c81629e6ec328ad9166a0a8b84e0c9
willian-mayer <willian.mayer.br@gmail.com>
2025-08-03 20:45:35 -0300

    editing footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@ import data from "../data/footer.json";
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white px-6 py-10 md:py-16 md:h-screen flex flex-col">
+    <footer className="bg-black text-white px-6 py-10 md:py-16 md:h-screen flex flex-col ">
       {/* Top Section - Title */}
-      <div className="flex-1 flex items-center justify-center md:justify-start mx-24">
+      <div className="flex-1 flex items-end justify-center md:justify-start mx-24">
         <h2 className="text-2xl md:text-4xl font-semibold leading-snug max-w-4xl mx-auto md:mx-0 text-center md:text-left">
           Starting a new project or looking for a new partner? Let’s talk &gt;{" "}
           <span className="underline">info@lynx3pl.com</span>
@@ -12,10 +12,10 @@ export default function Footer() {
       </div>
 
       {/* Divider */}
-      <div className="border-t-4 border-white my-6"></div>
+      <div className="border-t-4 border-white my-6 "></div>
 
       {/* Bottom Section - Footer Links */}
-      <div className="basis-1/3">
+      <div className="basis-1/4">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 text-sm text-center md:text-left">
           {data.map((section, index) => (
             <div key={index}>
