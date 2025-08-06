21177066f6c733d0a9edbcb25dd85cad95861f83
willian-mayer <willian.mayer.br@gmail.com>
2025-08-06 17:28:31 -0300

    new warehousing!

--- src/components/NewWarehousing.tsx
+++ src/components/NewWarehousing.tsx
@@ -9,7 +9,7 @@ export default function NewWarehousing() {
   return (
     <section
       ref={ref}
-      className="w-full bg-white md:px-6 sm:px-10 md:px-64 md:py-10 min-h-screen flex items-center"
+      className="w-full bg-white md:px-6 sm:px-10 xl:px-32 md:py-10 min-h-screen flex items-center"
     >
       {/* Mobile & Tablet: formato teléfono | Desktop: grid tipo Partners */}
       <div
@@ -20,16 +20,25 @@ export default function NewWarehousing() {
         "
       >
         {/* Imagen */}
-        <div className="flex items-center justify-center mb-6 md:mb-0 lg:row-span-3 md:col-start-2 md:col-span-2">
-          <motion.img
-            src="/newwarehousing/image.png"
-            alt="Warehousing"
-            className="w-full md:max-w-xl xl:max-w-4xl h-full shadow-md"
-            initial={{ opacity: 0, scale: 0.95 }}
-            animate={inView ? { opacity: 1, scale: 1 } : {}}
-            transition={{ duration: 0.6 }}
-          />
-        </div>
+<div className="flex items-center justify-center mb-6 md:mb-0 lg:row-span-3 md:col-start-2 md:col-span-2">
+  <motion.img
+    src="/newwarehousing/image.png"
+    alt="Warehousing"
+    className="
+      w-full 
+      md:max-w-lg      /* un poco más pequeño en desktop */
+      lg:max-w-lg      /* todavía más control en pantallas grandes */
+      xl:max-w-4xl     /* antes era 4xl, ahora lo bajamos */
+      h-auto 
+      shadow-md
+      object-contain   /* asegura que no se recorte raro */
+    "
+    initial={{ opacity: 0, scale: 0.95 }}
+    animate={inView ? { opacity: 1, scale: 1 } : {}}
+    transition={{ duration: 0.6 }}
+  />
+</div>
+
 
         {/* Title */}
         <div
@@ -37,8 +46,8 @@ export default function NewWarehousing() {
             px-6 
             flex items-center justify-center 
             text-left
-            md:text-left md:items-center md:justify-start md:row-span-3 md:col-start-1 
-            pt-6 sm:pt-12
+            md:text-left md:items-center md:justify-end md:row-start-3 md:col-start-1 
+            pt-6
           "
         >
           <motion.h2
@@ -60,7 +69,7 @@ export default function NewWarehousing() {
         {/* Descripción */}
         <div className="px-6 text-left md:text-left md:col-span-3 md:col-start-1 md:row-start-5 flex items-center justify-center md:mt-10">
           <motion.p
-            className="text-md sm:text-2xl text-black inter-medium"
+            className="text-md sm:text-[1.7em] text-black inter-medium"
             initial={{ opacity: 0, y: 10 }}
             animate={inView ? { opacity: 1, y: 0 } : {}}
             transition={{ duration: 0.6 }}
