a10b5853d01745bd7b5e9a4f8207106314a0fc5c
willian-mayer <willian.mayer.br@gmail.com>
2025-08-08 16:37:29 -0300

    new fulfillment phone

--- src/components/FulfillmentSecond.tsx
+++ src/components/FulfillmentSecond.tsx
@@ -9,14 +9,14 @@ export default function FulfillmentSecond() {
   return (
     <section
       ref={ref}
-      className="bg-white px-4 md:px-6 py-10 min-h-screen flex items-center justify-center overflow-y-auto"
+      className="bg-white md:px-6 py-10 min-h-screen flex items-center justify-center overflow-y-auto"
     >
       <div className="w-full max-w-6xl flex flex-col items-center md:mt-6">
         
         {/* Title + Description */}
-        <div className="text-center mb-8">
+        <div className="text-center mb-2">
           <motion.h2
-            className="text-md md:text-[2.2em] font-bold leading-tight pt-10 md:mx-24 md:pb-2"
+            className="text-[1.5em] md:text-[2.2em] font-bold leading-tight pt-10 md:mx-24 md:pb-2 mx-4"
             initial={{ opacity: 0, y: -20 }}
             animate={inView ? { opacity: 1, y: 0 } : {}}
             transition={{ duration: 0.5 }}
@@ -24,7 +24,7 @@ export default function FulfillmentSecond() {
             {title}
           </motion.h2>
           <motion.p
-            className="text-gray-800 text-xs md:text-xl font-medium leading-snug"
+            className="text-black text-xs md:text-xl leading-snug mx-26 text-left pl-8 my-4"
             initial={{ opacity: 0, y: -10 }}
             animate={inView ? { opacity: 1, y: 0 } : {}}
             transition={{ duration: 0.5, delay: 0.2 }}
@@ -37,26 +37,20 @@ export default function FulfillmentSecond() {
         <div className="w-full flex flex-col lg:flex-row items-center lg:items-center gap-2 md:px-18">
           
           {/* Left: Image */}
-          <div className="flex-shrink-0 flex justify-center flex-col lg:justify-center lg:items-center w-[240px] lg:w-[480px]">
+          <div className="flex-shrink-0 flex justify-center flex-col lg:justify-center lg:items-center w-full lg:w-[480px]">
             <motion.img
               src="/fulfillmentSecond/image.jpg"
               alt="Fulfillment Service"
-              className="w-full object-cover rounded-lg shadow-md mb-2 surface-duo-img md:ml-22"
+              className="w-full h-36 object-cover mb-2 surface-duo-img md:ml-22 md:cover-center object-bottom"
               initial={{ opacity: 0, scale: 0.95 }}
               animate={inView ? { opacity: 1, scale: 1 } : {}}
               transition={{ duration: 0.6 }}
             />
-                <a
-      href="#form"
-      className="bg-black text-white text-base text-sm px-2 font-bold lg:px-4 lg:py-2 rounded-full hover:bg-gray-900 transition whitespace-nowrap w-min border-2 border-black ml-5 lg:ml-20"
-    >
-      Looking for a new partner?
-    </a>
           </div>
 
           {/* Right: Benefits */}
-          <div className="flex-1 flex flex-col justify-center w-full md:pl-36">
-            <div className="space-y-3 ">
+          <div className="flex-1 flex flex-col justify-center w-full pl-36">
+            <div className="space-y-1 md:space-y-3 mr-8">
               {benefits.map((b, i) => (
                 <motion.div
                   key={i}
@@ -64,9 +58,9 @@ export default function FulfillmentSecond() {
                   animate={inView ? { opacity: 1, x: 0 } : {}}
                   transition={{ duration: 0.4, delay: 0.2 * i }}
                 >
-                  <h4 className="text-xs md:text-[1.1em] font-bold">
+                  <h4 className="text-[0.6em] md:text-[1.1em]">
                     ● {b.title}
-                    <span className="text-xs md:text-[1.1em] text-black font-normal leading-tight">
+                    <span className="text-[1em] md:text-[1.1em] text-black font-normal leading-tight">
                       : {b.desc.trim()}
                     </span>
                   </h4>
