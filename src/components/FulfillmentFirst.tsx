38d348c5941d967faea416af4a464a55ed0843fa
willian-mayer <willian.mayer.br@gmail.com>
2025-08-13 19:17:31 -0300

    new fulfillment first

--- src/components/FulfillmentFirst.tsx
+++ src/components/FulfillmentFirst.tsx
@@ -3,7 +3,7 @@ import { useInView } from "react-intersection-observer";
 import data from "../data/fulfillmentFirst.json";
 
 export default function FulfillmentFirst() {
-  const { title, desc } = data;
+  const { title } = data;
   const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.3 });
 
   return (
@@ -20,7 +20,7 @@ export default function FulfillmentFirst() {
         "
       >
         {/* Imagen (primero en escritorio) */}
-        <div className="relative flex items-center justify-center md:w-1/2 max-h-[450px] md:rounded-xl overflow-hidden order-2 md:order-1">
+        <div className="relative flex items-center justify-center md:w-1/2 max-h-[450px] md:rounded-xl overflow-hidden order-2 md:order-1 md:ml-6">
           <motion.img
             src="/fulfillmentFirst/image.jpg"
             alt={title}
@@ -35,27 +35,38 @@ export default function FulfillmentFirst() {
             </p>
           </div>
         </div>
+{/* Texto (primero en móvil, segundo en escritorio) */}
+<div className="
+  flex flex-col md:w-1/2 order-1 md:order-2
+  justify-start md:justify-end
+  h-auto md:h-full
+  md:mt-40
+  md:mr-5
+  md:mb-2
+">
+  <motion.h2
+    className="text-3xl md:text-6xl font-bold text-black mb-4 md:mb-16 md:mt-0 text-right mr-4 md:mr-0"
+    initial={{ opacity: 0, y: -20 }}
+    animate={inView ? { opacity: 1, y: 0 } : {}}
+    transition={{ duration: 0.6 }}
+  >
+    {title}
+  </motion.h2>
+<motion.p
+  className="text-base md:text-xl text-black md:mx-0 md:px-0 text-right ml-20 w-62 md:w-full md:ml-0 mr-4 md:mr-0"
+  initial={{ opacity: 0, y: -10 }}
+  animate={inView ? { opacity: 1, y: 0 } : {}}
+  transition={{ duration: 0.6, delay: 0.2 }}
+>
+  Managing fulfillment can be stressful and{" "}
+  time<span className="inline md:block">consuming.</span>{" "}
+  <span className="inline md:block">We take care of</span>{" "}
+  <span className="inline md:block"></span>{" "}
+  inventory, packing, and shipping,<span className="inline md:block"></span> so you can
+  focus on growing your business.
+</motion.p>
+</div>
 
-        {/* Texto (primero en móvil, segundo en escritorio) */}
-        <div className="flex flex-col md:w-1/2 order-1 md:order-2">
-          <motion.h2
-            className="text-3xl md:text-4xl font-bold text-black mb-4 md:mt-0 text-right"
-            initial={{ opacity: 0, y: -20 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.6 }}
-          >
-            {title}
-          </motion.h2>
-
-          <motion.p
-            className="text-base md:text-lg text-gray-700 md:mx-0 mb-2 px-4 md:px-0 text-right"
-            initial={{ opacity: 0, y: -10 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.6, delay: 0.2 }}
-          >
-            {desc}
-          </motion.p>
-        </div>
       </div>
     </section>
   );
