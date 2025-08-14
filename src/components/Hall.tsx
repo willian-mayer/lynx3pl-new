3bad5503651e52cb948300f2bf2e25532a736fa3
willian-mayer <willian.mayer.br@gmail.com>
2025-08-14 17:32:10 -0300

    new

--- src/components/Hall.tsx
+++ src/components/Hall.tsx
@@ -11,18 +11,19 @@ export default function Hall() {
       ref={ref}
       className="
         bg-white 
-        px-4 md:px-10 
+         md:px-10 
         py-10 
         min-h-screen 
         flex flex-col 
         items-center 
         text-center 
         justify-center
+        md:pb-0
       "
     >
       {/* Title */}
       <motion.h2
-        className="text-2xl md:text-4xl font-bold mb-8 max-w-4xl"
+        className="text-2xl md:text-[1.7em] font-bold mb-10 mx-23 text-left pl-4"
         initial={{ opacity: 0, y: -20 }}
         animate={inView ? { opacity: 1, y: 0 } : {}}
         transition={{ duration: 0.5 }}
@@ -31,12 +32,12 @@ export default function Hall() {
       </motion.h2>
 
       {/* Image + Button */}
-      <div className="relative w-full max-w-5xl">
+      <div className="relative w-full ">
         {/* Image */}
         <motion.img
           src={imgUrl}
           alt="Hall Image"
-          className="w-full h-auto rounded-xl shadow-md"
+          className="w-full h-auto md:rounded-xl shadow-md md:max-w-4xl md:mx-auto"
           initial={{ opacity: 0, y: 20 }}
           animate={inView ? { opacity: 1, y: 0 } : {}}
           transition={{ duration: 0.5 }}
@@ -48,11 +49,12 @@ export default function Hall() {
           className="
             absolute left-1/2 -translate-x-1/2 
             bottom-0 translate-y-1/2
-            bg-black text-white px-6 py-3 
-            rounded-lg shadow-lg 
+            bg-black text-white px-4 py-2 
+            rounded-full shadow-lg 
             hover:bg-gray-800 transition
-            w-auto md:w-auto
-            max-md:w-[90%] max-md:text-sm
+            w-max md:w-auto
+            max-md:text-sm
+            md:font-semibold
           "
           initial={{ opacity: 0, y: 20 }}
           animate={inView ? { opacity: 1, y: 0 } : {}}
