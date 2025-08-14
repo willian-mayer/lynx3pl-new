b638be02654ce31d7379213a9f4ed8911779e023
willian-mayer <willian.mayer.br@gmail.com>
2025-08-14 11:43:29 -0300

    new v8

--- src/components/FulfillmentSecond.tsx
+++ src/components/FulfillmentSecond.tsx
@@ -14,7 +14,7 @@ export default function FulfillmentSecond() {
       <div className="w-full max-w-6xl flex flex-col md:grid md:grid-cols-2 gap-6 items-center h-full">
 
         {/* Columna 1: Imagen + Título con orden adaptable */}
-        <div className="flex flex-col md:flex-row items-center md:items-start px-4 md:mb-18 gap-6">
+        <div className="flex flex-col md:flex-row items-center md:items-start md:mb-18 gap-6">
           
           {/* Imagen (segundo en móvil, primero en escritorio) */}
           <div className="flex justify-center items-center overflow-hidden md:h-[220px] h-[100px] order-2 md:order-1">
@@ -36,7 +36,7 @@ export default function FulfillmentSecond() {
           {/* Texto (primero en móvil, segundo en escritorio) */}
           <div className="order-1 md:order-2">
             <motion.h2
-              className="text-[1.5em] md:text-4xl font-bold leading-tight mb-4 md:mr-5 mt-10 md:mt-0 text-center md:text-left md:flex-1"
+              className="text-[1.2em] md:text-4xl font-bold leading-tight mb-4 md:mr-5 mt-10 md:mt-0 text-center md:text-left md:flex-1"
               initial={{ opacity: 0, y: -20 }}
               animate={inView ? { opacity: 1, y: 0 } : {}}
               transition={{ duration: 0.5 }}
@@ -46,7 +46,7 @@ export default function FulfillmentSecond() {
 
             {/* Descripción solo en móvil */}
             <motion.p
-              className="text-black text-xs leading-snug md:hidden ml-35 mr-10"
+              className="text-black text-[0.58em] leading-snug md:hidden ml-35 mr-4"
               initial={{ opacity: 0, y: -10 }}
               animate={inView ? { opacity: 1, y: 0 } : {}}
               transition={{ duration: 0.5, delay: 0.2 }}
@@ -66,7 +66,7 @@ export default function FulfillmentSecond() {
                 animate={inView ? { opacity: 1, x: 0 } : {}}
                 transition={{ duration: 0.4, delay: 0.2 * i }}
               >
-                <h4 className="text-[0.61em] md:text-[1.05em] mr-2 ml-34 md:mr-0 md:ml-0">
+                <h4 className="text-[0.58em] md:text-[1.05em] ml-30 md:mr-0 md:ml-0">
                   ● {b.title}
                   <span className="text-black font-normal">
                     : {b.desc.trim()}
