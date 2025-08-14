e5acf43b9c3dd67d6d01fee1aaf4605c1d9080d5
willian-mayer <willian.mayer.br@gmail.com>
2025-08-14 15:26:22 -0300

    new

--- src/components/Transloading.tsx
+++ src/components/Transloading.tsx
@@ -15,31 +15,14 @@ export default function Transloading() {
         className="
           w-full
           flex flex-col items-center justify-center 
-          lg:grid lg:grid-rows-[auto_1fr] lg:grid-cols-1 lg:gap-8 lg:mx-24 lg:mt-14
+          lg:grid lg:grid-cols-1 lg:grid-rows-[auto_1fr] lg:gap-8 lg:mx-24 lg:mt-14 mt-5
         "
       >
-        {/* Imagen */}
-        <div className="flex items-center justify-center mb-6 lg:mb-0 lg:col-span-1">
-          <motion.img
-            src={imgUl}
-            alt={title}
-            className="
-              w-full 
-              lg:h-[280px] 
-              object-cover 
-              object-center
-            "
-            initial={{ opacity: 0, scale: 0.95 }}
-            animate={inView ? { opacity: 1, scale: 1 } : {}}
-            transition={{ duration: 0.6 }}
-          />
-        </div>
-
-        {/* Lista */}
-        <div className="w-full grid grid-cols-1 gap-2 px-2">
+        {/* Texto */}
+        <div className="w-full grid grid-cols-1 gap-2 px-18 mb-6 lg:mb-0">
           {/* Título */}
           <motion.h2
-            className="text-2xl font-bold text-black inter-bold mb-4"
+            className="text-md font-bold text-black inter-bold mb-4"
             initial={{ opacity: 0, y: -10 }}
             animate={inView ? { opacity: 1, y: 0 } : {}}
             transition={{ duration: 0.6 }}
@@ -55,12 +38,40 @@ export default function Transloading() {
               animate={inView ? { opacity: 1, y: 0 } : {}}
               transition={{ duration: 0.5, delay: index * 0.1 }}
             >
-              <h3 className="text-md font-normal text-black inter">
+              <h3 className="text-[0.8em] font-normal text-black inter mr-20">
                 • {item.name}
               </h3>
             </motion.div>
           ))}
         </div>
+
+{/* Imagen */}
+<div className="flex items-center justify-center lg:col-span-1">
+  <motion.img
+    src={imgUl}
+    alt={title}
+    className="
+      h-[120px]
+      w-[420px]
+      object-fill
+      lg:h-[280px] lg:object-cover
+      object-center
+    "
+    initial={{ opacity: 0, scale: 0.95 }}
+    animate={inView ? { opacity: 1, scale: 1 } : {}}
+    transition={{ duration: 0.6 }}
+  />
+</div>
+
+                  {/* Botón */}
+          <motion.button
+            className="mt-1 bg-black text-white px-6 py-2 rounded-xl font-medium hover:bg-gray-800 transition text-xs"
+            initial={{ opacity: 0, y: 10 }}
+            animate={inView ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 0.5, delay: whyUs.length * 0.1 }}
+          >
+            Contact Us for a Custom Quote​
+          </motion.button>
       </div>
     </section>
   );
