9f36443c808c940fdc8f58559aa581e4d8dcc253
willian-mayer <willian.mayer.br@gmail.com>
2025-08-14 17:22:20 -0300

    new

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -13,7 +13,7 @@ export default function Reviews() {
     Array.from({ length: count }).map((_, i) => (
       <Star
         key={i}
-        className="w-2 h-2 text-red-600 fill-red-600 inline-block mr-[1px] mt-1"
+        className="w-2 h-2 text-red-600 fill-red-600 inline-block mr-[1px] mt-1 md:w-3 md:h-3"
       />
     ));
 
@@ -34,54 +34,48 @@ export default function Reviews() {
         pt-20
       "
     >
-      <div
-        className="
-          w-full max-w-6xl mx-auto
-          flex flex-col md:flex-row md:items-stretch
-          gap-2
-        "
-      >
-        {/* Left column - Main Review */}
+      <div className="w-full max-w-6xl mx-auto flex flex-col md:flex-row md:items-stretch gap-4">
+        {/* Left column - Main Review + Image */}
         <motion.div
-          className="flex items-center justify-center"
+          className="flex flex-col items-center md:items-start md:w-3/5 "
           initial={{ opacity: 0, y: -20 }}
           animate={inView ? { opacity: 1, y: 0 } : {}}
           transition={{ duration: 0.5 }}
         >
-          {/* Imagen para móviles (antes de md) */}
+          {/* Imagen */}
           <img
             src="/review/quote.png"
             alt="Quote Mobile"
-            className="block md:hidden w-auto object-contain"
+            className="block md:hidden w-auto object-contain mb-4"
           />
-
-          {/* Imagen para md+ */}
           <img
             src="/review/quote-md.png"
             alt="Quote Desktop"
-            className="hidden md:block w-full max-w-md object-contain"
+            className="hidden md:block w-full object-contain"
           />
+          {/* Autor */}
+          <p className="text-[1em] text-red-700 font-semibold text-center md:text-right ml-9 md:ml-25 md:text-xl">
+            — {mainReview.author}, {mainReview.country} ({mainReview.year})
+          </p>
         </motion.div>
-        <p className="text-sm text-red-700 font-semibold text-center md:text-right md:mr-20 ml-10">
-          — {mainReview.author}, {mainReview.country} ({mainReview.year})
-        </p>
+
         {/* Right column - Other Reviews */}
-        <div className="md:w-[40%] flex flex-col justify-end lg:ml-10">
+        <div className="md:w-2/5 flex flex-col justify-start gap-4 ml-24 md:ml-0 md:mt-32">
           {reviews.map((r, i) => (
             <motion.div
               key={i}
-              className="flex flex-col items-start my-1"
+              className="flex flex-col items-start"
               initial={{ opacity: 0, x: 30 }}
               animate={inView ? { opacity: 1, x: 0 } : {}}
               transition={{ duration: 0.4, delay: 0.1 * i }}
             >
-              <div className="flex items-start ml-25 mr-7">
-                <div className="flex mr-2 mb-2">{renderStars(r.stars)}</div>
-                <p className="text-black text-[0.6em] md:text-[1em] font-semibold mb-2">
+              <div className="flex items-start mr-5 md:mr-0 md:ml-7">
+                <div className="flex mr-2 mb-1">{renderStars(r.stars)}</div>
+                <p className="text-black text-[0.55em] md:text-[0.85em] font-semibold mb-4">
                   “{r.review}”
                 </p>
               </div>
-              <p className="text-[0.6em] lg:text-[0.9em] text-red-700 font-semibold pl-6 md:my-2 ml-30 mr-8">
+              <p className="text-[0.55em] lg:text-[0.8em] text-red-700 font-semibold mr-8 pl-14 md:ml-15 md:mr-30 ">
                 — {r.author}, {r.country} ({r.year})
               </p>
             </motion.div>
