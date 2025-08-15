a4879c79d13f463cc26087cbde0bd433e7e789a0
willian-mayer <willian.mayer.br@gmail.com>
2025-08-15 18:12:09 -0300

    new

--- src/components/Entrance.tsx
+++ src/components/Entrance.tsx
@@ -18,7 +18,7 @@ export default function Entrance() {
       <div className="absolute top-13 right-30 flex flex-col items-enter space-y-2 text-white invisible md:visible">
         {languages.map((lang) => (
           <div key={lang.short} className="flex items-center space-x-2 hover:underline cursor-pointer">
-            <img src="/item.png" alt="icon" className="h-4 w-auto" />
+            <img src="/logo-ico.png" alt="icon" className="h-4 w-auto" />
             <span className="text-sm font-medium">{lang.short}</span>
           </div>
         ))}
