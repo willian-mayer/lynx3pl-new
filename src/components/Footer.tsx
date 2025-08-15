bb4b7cbb106a30c581db9f740d6769c675e3252b
willian-mayer <willian.mayer.br@gmail.com>
2025-08-14 21:28:10 -0300

    new

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,25 +2,33 @@ import data from "../data/footer.json";
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white px-6 py-10 md:py-16 md:h-screen flex flex-col ">
+    <footer className="bg-black text-white py-10 md:py-16 md:h-screen flex flex-col justify-end h-full">
       {/* Top Section - Title */}
-      <div className="flex-1 flex items-end justify-center md:justify-start mx-24">
-        <h2 className="text-2xl md:text-4xl font-semibold leading-snug max-w-4xl mx-auto md:mx-0 text-center md:text-left">
-          Starting a new project or looking for a new partner? Let’s talk &gt;{" "}
-          <span className="underline">info@lynx3pl.com</span>
+      <div className="flex-1 flex items-end justify-center md:justify-start mx-4 md:mx-24 mr-44 ml-10 mx-6">
+        <h2 className="text-lg md:text-4xl font-semibold leading-snug max-w-4xl mx-auto md:mx-0 text-center text-left">
+          Starting a new project or looking for a new partner?{" "}
+          <span className="underline">Let’s talk</span> &gt;{" "}
+          <span>info@lynx3pl.com</span>
         </h2>
       </div>
 
       {/* Divider */}
-      <div className="border-t-4 border-white my-6 "></div>
+      <div className="border-t-4 border-white my-6 w-full"></div>
 
       {/* Bottom Section - Footer Links */}
-      <div className="basis-1/4">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 text-sm text-center md:text-left">
+      <div className="basis-1/4 px-2 md:px-6 md:ml-20 mx-8 ">
+        <div
+          className="
+            grid grid-cols-2 md:grid-cols-4 
+            text-[0.5em] md:text-sm 
+            text-left md:text-left 
+            gap-2 md:gap-0
+          "
+        >
           {data.map((section, index) => (
             <div key={index}>
-              <h3 className="text-lg font-bold mb-3">{section.title}</h3>
-              <ul className="space-y-2">
+              <h3 className="text-sm md:text-lg font-bold mb-3">{section.title}</h3>
+              <ul className="md:space-y-2">
                 {section.items.map((item, i) => (
                   <li key={i}>
                     <a href="#" className="hover:underline">
